0267d1d6a439ad51da35507fd1e57554710441d0
Adarsha Khatua <adarshakhatua.ak@gmail.com>
2022-07-29 22:24:57 +0530

    create post modal done......Working on  image upload fronend

--- front-end/src/components/createPost.feed.jsx
+++ front-end/src/components/createPost.feed.jsx
@@ -1,71 +1,141 @@
-import { useState } from "react";
+import {  useEffect, useState } from "react";
 import "../style/createPost.feed.css";
-import { Photo, Video, Event, Article,Earth } from "./custom.icon";
+import { Photo, Video,Document,Job,Celebrate,Poll,More,Comment, Event, Article,Earth } from "./custom.icon";
 import { ProfileImage } from "./profileImage";
 import { GrClose } from "react-icons/gr";
 import { IoMdArrowDropdown } from "react-icons/io"
 
 export const CreatePostFeed = () => {
+
     const [pop, setPop] = useState(false);
+
     const handlePop = () => {
         setPop(!pop);
     }
+    //modal functionality......
+    useEffect(() => {
+        if (pop) {
+            document.body.style.overflow = "hidden";
+            document.getElementById("outerLayout").style.backgroundColor ="rgb(0 0 0 / 75%)"
+        }
+        else {
+            document.body.style.overflow = "visible";
+        }
+        document.getElementById("outerLayout")?.addEventListener("click", (e) => { if (!e.target.closest("#CreatePostPopDiv")) { setPop(false); console.log(e.target.className) } })
+    }, [pop])
+    
+    
+
     return (
-        <div id="createPostFeed">
-            <div id="createPostFeed1">
-               
-                <ProfileImage
-                    image={"https://media-exp1.licdn.com/dms/image/C4D03AQE732pYOz74lw/profile-displayphoto-shrink_100_100/0/1649706029434?e=1663804800&v=beta&t=QuJxqFe_hwWQtwemok7NeSjOANgS0YHYrXqqdnuHbdw"}
-                    imageStyle={{ height: "48px", width: "48px", top: "0px" }}
-                />
-                <div id="createrInputDiv" onClick={handlePop}>Start a post about a topic that excites you</div>
-            </div>
+        <>
+            {!pop && <div id="createPostFeed">
+                <div id="createPostFeed1">
 
-            <div id="createPostFeed2">
-                <div id="createPostPhotoDiv" className="createPostComponent"><Photo /><p>Photo</p></div>
-                <div id="createPostVideoDiv" className="createPostComponent"><Video /><p>Photo</p></div>
-                <div id="createPostEventDiv" className="createPostComponent"><Event /><p>Event</p></div>
-                <div id="createPostWriteArticleDiv" className="createPostComponent"><Article /><p>Write article</p></div>
-            </div>
-            
-            <CreatePostPop/>
+                    <ProfileImage
+                        image={"https://media-exp1.licdn.com/dms/image/C4D03AQE732pYOz74lw/profile-displayphoto-shrink_100_100/0/1649706029434?e=1663804800&v=beta&t=QuJxqFe_hwWQtwemok7NeSjOANgS0YHYrXqqdnuHbdw"}
+                        imageStyle={{ height: "48px", width: "48px", top: "0px" }}
+                    />
+                    <div id="createrInputDiv" onClick={handlePop}>Start a post about a topic that excites you</div>
+                </div>
 
-        </div>
+                <div id="createPostFeed2">
+                    <div id="createPostPhotoDiv" className="createPostComponent"><Photo /><p>Photo</p></div>
+                    <div id="createPostVideoDiv" className="createPostComponent"><Video /><p>Photo</p></div>
+                    <div id="createPostEventDiv" className="createPostComponent"><Event /><p>Event</p></div>
+                    <div id="createPostWriteArticleDiv" className="createPostComponent"><Article /><p>Write article</p></div>
+                </div>
+            </div>}
+            {pop && <CreatePostPop handleClick={handlePop}/>}
+        </>
+
     )
 }
 
 
 
-const CreatePostPop = () => {
+const CreatePostPop = ({handleClick}) => {
+   
+    
+    const handleCreatePostInputHeight = (e) => {
+        const inpt = document.getElementById("CreatePostPop_Input");
+        const popContent = document.getElementById("popDivContent");
+
+        inpt.style.height = "96px";
+        let scrlHeight = e.target.scrollHeight;
+        inpt.style.height = `${scrlHeight}px`;
+
+        if (popContent.offsetHeight < Math.floor(window.innerHeight*0.5)) {
+            popContent.className ="scrollVisible"
+        }
+        else {
+            popContent.className = "";
+        }
+        console.log(window.innerHeight, popContent.offsetHeight)
+       
+    }
+
+    const [title, setTitle] = useState("Create a post");
+    const [image, setImage] = useState(null);
+
+    const imagePreview = (e) => {
+        const imageUrl = URL.createObjectURL(e.target.files[0]);
+        setImage(imageUrl);
+    }
+
     return (
-        <div id="CreatePostPopDiv">
+        <div id="outerLayout">
+            <div id="CreatePostPopDiv">
 
-            <div id="CreatePostPopDivTop">
-                Create a post
-                <div id="closeBtn"><GrClose /></div>
-            </div>
+                <div id="CreatePostPopDivTop" >
+                    {title}
+                    <div id="closeBtn" onClick={handleClick}><GrClose /></div>
+                </div>
 
-            <div id="popDivContent">
-                <div id="CreatePostPopDivProfile">
-                    <ProfileImage
-                        image={"https://media-exp1.licdn.com/dms/image/C4D03AQE732pYOz74lw/profile-displayphoto-shrink_100_100/0/1649706029434?e=1663804800&v=beta&t=QuJxqFe_hwWQtwemok7NeSjOANgS0YHYrXqqdnuHbdw"}
-                        imageStyle={{ height: "48px", width: "48px", }}
-                    />
-                    <div id="dropDownDiv">
-                        Adarsha Khatua
-                        <div id="dropDownBtn">
-                            <Earth />
-                            Anyone
-                            <IoMdArrowDropdown />
+                {title === "Create a post" ? <div id="popDivContent" className="scrollVisible">
+                    <div id="CreatePostPopDivProfile">
+                        <ProfileImage
+                            image={"https://media-exp1.licdn.com/dms/image/C4D03AQE732pYOz74lw/profile-displayphoto-shrink_100_100/0/1649706029434?e=1663804800&v=beta&t=QuJxqFe_hwWQtwemok7NeSjOANgS0YHYrXqqdnuHbdw"}
+                            imageStyle={{ height: "48px", width: "48px", }}
+                        />
+                        <div id="dropDownDiv">
+                            Adarsha Khatua
+                            <div id="dropDownBtn">
+                                <Earth />
+                                Anyone
+                                <IoMdArrowDropdown />
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                <div id="CreatePostPopInput">
-                    <textarea name="" id="" cols="30" rows="10" placeholder="What do you want to talk"></textarea>
-                </div>
-           </div>
+                    <div id="CreatePostPopInput">
+                        <textarea name="" onKeyUp={handleCreatePostInputHeight} id="CreatePostPop_Input" cols="30" rows="10" placeholder="What do you want to talk" ></textarea>
+                    </div>
+                </div> : <><div id="imagePreview"><img src={image} alt="" /></div></>}
 
+                {image===null?<div id="CreatePostPopbottom">
+
+                    <div>
+                        <form action="" encType="multipart/form-data">
+                            <input type="file" name="photo" id="photo" style={{ display: "none" }} onChange={imagePreview} />
+                            <label htmlFor="photo" onClick={() => { setTitle("Edit your photo") }}><Photo /></label>
+                            <input type="file" name="" id="video" style={{ display: "none" }} />
+                            <label htmlFor="video"><Video /></label>
+                        </form>
+                        <Document />
+                        <Job />
+                        <Celebrate />
+                        <Poll />
+                        <More />
+                    </div>
+
+                    <div>
+                        <Comment />
+                        Anyone
+                    </div>
+
+                    <div disabled>Post</div>
+                </div> : <><div id="imagePreviewBtn"><button onClick={() => { setTitle("Edit your photo") }}>Done</button></div></>}
+            </div>
         </div>
     )
 }
--- front-end/src/components/custom.icon.jsx
+++ front-end/src/components/custom.icon.jsx
@@ -221,27 +221,35 @@ export const Imogi = ({style}) => {
         </svg>
     )
 }
-// export const Celebrate_Reaction = () => {
-//     return (
-        
-//     )
-// }
-// export const Support_Reaction = () => {
-//     return (
-        
-//     )
-// }
+export const Document = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" class="mercado-match" width="24" height="24" focusable="false">
+            <path d="M3 3v15a3 3 0 003 3h9v-6h6V3zm9 8H6v-1h6zm6-3H6V7h12zm-2 8h5l-5 5z"></path>
+        </svg> 
+    )
+}
+export const Celebrate = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" class="mercado-match" width="24" height="24" focusable="false">
+            <path d="M22 11.1L20.47 10a1.09 1.09 0 01-.4-1.25l.62-1.81a1.11 1.11 0 00-.7-1.4 1.07 1.07 0 00-.35-.06h-2a1.09 1.09 0 01-1.05-.76l-.59-2A1.09 1.09 0 0015 2a1.11 1.11 0 00-.66.22l-1.69 1.17a1.13 1.13 0 01-1.31 0L9.75 2.22a1.11 1.11 0 00-1.55.16 1.07 1.07 0 00-.2.38L7.41 4.7a1.09 1.09 0 01-1 .76h-2a1.11 1.11 0 00-1.16 1.06 1.34 1.34 0 00.06.4l.63 1.82a1.1 1.1 0 01-.4 1.26L2 11.11a1.1 1.1 0 00-.26 1.53 1.28 1.28 0 00.26.26L3.53 14a1.09 1.09 0 01.4 1.25l-.62 1.8a1.11 1.11 0 00.7 1.4 1.07 1.07 0 00.35.06h2a1.09 1.09 0 011 .76l.64 2a1.12 1.12 0 001.1.73 1.05 1.05 0 00.64-.22l1.6-1.17a1.1 1.1 0 011.31 0l1.6 1.17a1.14 1.14 0 001.75-.55l.62-1.93a1.11 1.11 0 011.05-.76h2a1.11 1.11 0 001.11-1.11 1 1 0 00-.06-.35l-.63-1.82a1.11 1.11 0 01.38-1.26L22 12.89a1.07 1.07 0 00.5-.89 1.1 1.1 0 00-.5-.9zM7 11v-1h10v1zm2 3v-1h6v1z"></path>
+        </svg>
+    )
+}
 
-// export const Funny_Reaction = () => {
-//     return (
-        
-//     )
-// }
-// export const Love_Reaction = () => {
-//     return (
-        
-//     )
-// }
+export const Poll = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" class="mercado-match" width="24" height="24" focusable="false">
+            <path d="M23 20v1H1v-1zM8 9H2v10h6zm7-6H9v16h6zm7 11h-6v5h6z"></path>
+        </svg>
+    )
+}
+export const More = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" class="mercado-match" width="24" height="24" focusable="false">
+            <path d="M14 12a2 2 0 11-2-2 2 2 0 012 2zM4 10a2 2 0 102 2 2 2 0 00-2-2zm16 0a2 2 0 102 2 2 2 0 00-2-2z"></path>
+        </svg>
+    )
+}
 // export const Insightful_Reaction = () => {
 //     return (
         
